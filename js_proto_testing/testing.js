bdc51a0ee964e09b4d0377aaf184591b08ae65ed
Madhava Jay <me@madhavajay.com>
2022-01-13 13:01:25 +1000

    Added Serialize from JS to Python
    - added shell script to run server
    - added requirements.txt

--- js_proto_testing/testing.js
+++ js_proto_testing/testing.js
@@ -1,18 +1,35 @@
-import fetch from "node-fetch"
-import protobuf from "protobufjs"
+import fetch from "node-fetch";
+import protobuf from "protobufjs";
 
-const root = await protobuf.load("./proto/tensor.proto")
+const API_URL = "http://127.0.0.1:5001";
 
-
-// // Obtain a message type
+// get protobuf definition
+const root = await protobuf.load("./proto/tensor.proto");
 var tensor_pb = root.lookupType("syft.Tensor");
-console.log("Loading", tensor_pb)
+console.log("Tensor PB", tensor_pb);
 
+// get protobuf message bytes from server
+const response = await fetch(`${API_URL}/rcv`);
+const bytes = await response.arrayBuffer();
+const data = new Uint8Array(bytes);
+console.log("Bytes", bytes, "Data", data);
 
-console.log("Outside", tensor)
-const response = await fetch("http://localhost:5000/rcv") 
-const bytes =  await response.arrayBuffer()
-const data = new Uint8Array(bytes)
-console.log("Bytes", bytes, "Data", data)
-var tensor = tensor_pb.decode(data)
-console.log("Tensor", tensor)
+// decode into Tensor object
+var tensor = tensor_pb.decode(data);
+console.log("Tensor", tensor);
+
+// change data
+tensor.tagName = "y";
+tensor.bad = "no";
+
+console.log("final output", tensor);
+// Encode a message to an Uint8Array (browser) or Buffer (node)
+var buffer = tensor_pb.encode(tensor).finish();
+console.log("output", buffer);
+// ... do something with buffer
+
+const response2 = await fetch(`${API_URL}/send`, {
+  method: "POST",
+  headers: {"content-type": "application/octect-stream"},
+  body: buffer,
+});
--- js_proto_testing/testing.py
+++ js_proto_testing/testing.py
@@ -1,15 +1,18 @@
+# third party
 from flask import Flask
 from flask import request
-from flask import Response
 from python_proto.tensor_pb2 import Tensor as Tensor_PB
 
 app = Flask(__name__)
 
+
 def make_tensor(tag, dtype):
     return Tensor_PB(tag_name=tag, public_dtype=dtype)
 
+
 @app.route("/rcv")
 def message():
+    print("RECIEVE")
     x = make_tensor("x", "np.int32")
     print(type(x))
     blob = x.SerializeToString()
@@ -17,9 +20,17 @@ def message():
     headers = {"content-type": "application/octect-stream"}
     return blob, 200, headers
 
+
 @app.route("/send", methods=["POST"])
 def rcv_message():
+    print("SEND")
+    print(request.headers)
     body = request.get_data()
-    print(body)
-    print(type(body))
+    print("data", body)
+    print("type", type(body))
+    tensor = Tensor_PB()
+    print("Tensor_PB", tensor, type(tensor))
+    tensor.ParseFromString(body)
+    print("type", tensor)
+    print("tensor", tensor)
     return "Echo"
