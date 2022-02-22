f3413a064421fce02033983ff9da294c7a1aa732
Shubham Gupta <shubhamgupta3121@gmail.com>
2022-02-22 11:43:59 +0530

    seaweed: added method to combine the chunks

--- packages/file_system/seaweed_benchmark.py
+++ packages/file_system/seaweed_benchmark.py
@@ -1,10 +1,11 @@
+import json
 from weed.master import WeedMaster
 from weed import operation
 from io import BytesIO
 from datetime import datetime
 import matplotlib.pyplot as plt
 import gc
-import sys
+import requests
 
 client  = WeedMaster()
 wo = operation.WeedOperation()
@@ -27,25 +28,56 @@ def create_binary_data(length):
     return chunks
 
 
+def combine_chunks(fids, file_name, length):
+    manifest = {
+        "name": f"{file_name}-sample.txt",
+        "mime": "text/html",
+        "size": length,
+        "chunks": fids
+    }
 
-def send_data(length):
+    new_fid = wo.acquire_new_fids()
+    fid_url = wo.get_fid_full_url(new_fid[0])
+    params = (
+        ('cm', 'true'),
+        ('pretty', 'yes'),
+    )
+
+    files = {
+        'file': ('manifest.json', json.dumps(manifest)),
+    }
+    response = requests.post(fid_url, params=params, files=files)
+    print(f"Combine status: {response.status_code}")
+    return new_fid
+
+
+def send_data(filename, length):
     chunks = create_binary_data(length)
     gc.collect()
     before = datetime.now()
     fids = []
+    offset = 0
     for chunk in chunks:
-        fids.append( wo.put(chunk).fid )
+        fid = wo.put(chunk).fid
+        fids.append({"fid": fid, "offset": offset, "size": chunk_size})
+        offset += chunk_size
+
+    fids = combine_chunks(fids, filename, length)
     after = datetime.now()
     diff = after - before
+    del chunks
+    gc.collect()
     return diff.total_seconds(), fids
 
 def get_data(fids):
     before = datetime.now()
     obj = b''
     for fid in fids:
-        wo.get_content(fid=fid)
+        data = wo.get(fid=fid)
     after = datetime.now()
     diff = after - before
+    del data
+    gc.collect()
     return diff.total_seconds()
 
 
@@ -76,7 +108,7 @@ delete_timestamps = {}
 for file_size in files:
     print("Testing : ", file_size[0])
     print("Sending ... ")
-    send_timestamp, fids = send_data(file_size[1])
+    send_timestamp, fids = send_data(file_size[0], file_size[1])
     gc.collect()
     print("Getting ...")
     get_timestamp = get_data(fids)
